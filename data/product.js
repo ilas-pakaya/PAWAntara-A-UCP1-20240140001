4e6ea8ffa750dc5d83efe0776da1028bf3c8bdda
MOJI <apple@apples-MacBook-Air.local>
2026-08-07 09:35:33 +0700

    Sprint1-add semantic HTML for beranda page

---
+++ data/product.js
@@ -0,0 +1,53 @@
+const products = [
+  {
+    id: 1,
+    name: "Beras Premium 5kg",
+    category: "sembako",
+    price: 65000,
+    stock: 20
+  },
+  {
+    id: 2,
+    name: "Minyak Goreng 2L",
+    category: "sembako",
+    price: 34000,
+    stock: 15
+  },
+  {
+    id: 3,
+    name: "Gula Pasir 1kg",
+    category: "sembako",
+    price: 16000,
+    stock: 30
+  },
+  {
+    id: 4,
+    name: "Telur Ayam 1kg",
+    category: "sembako",
+    price: 28000,
+    stock: 25
+  },
+  {
+    id: 5,
+    name: "Tepung Terigu 1kg",
+    category: "sembako",
+    price: 12000,
+    stock: 18
+  },
+  {
+    id: 6,
+    name: "Kopi Bubuk 200gr",
+    category: "minuman",
+    price: 15000,
+    stock: 40
+  },
+  {
+    id: 7,
+    name: "Teh Celup Kotak",
+    category: "minuman",
+    price: 8000,
+    stock: 50
+  }
+];
+
+module.exports = products;
